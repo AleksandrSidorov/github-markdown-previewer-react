da3f9e3d68e2fb806d3760e3c76d335826cb1d9f
Aleksandr Sidorov <shitl80@gmail.com>
2016-09-30 22:21:12 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'This is *awesome*!'};
+  }
+
+  handleSource (event) {
+    this.setState({ value: event.target.value }); 
+  }
+
+  setOutput () {
+    return { __html: `${this.state.value}` };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub Markup Previewer</h1>
+        <h3>Source</h3>
+        <textarea 
+          type="text" 
+          defaultValue={this.state.value} 
+          onChange={this.handleSource.bind(this)} 
+        />
+        <h3>Output</h3>
+        <div dangerouslySetInnerHTML={this.setOutput()} />
+      </div>
+    );
+  }
+}
+
+export default App;
